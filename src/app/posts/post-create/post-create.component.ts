80b44bcb300330967423ee9c5d35d1d21960f290
moin2672 <moin2672@gmail.com>
2020-09-29 07:33:44 +0530

    7. Adding PostService

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {Post} from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -11,8 +12,8 @@ export class PostCreateComponent implements OnInit {
 
   enteredTitle="";
   enteredContent="";
-  @Output() postCreated =  new EventEmitter<Post>();
-  constructor() { }
+
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
   }
@@ -22,7 +23,7 @@ export class PostCreateComponent implements OnInit {
       return;
     }
     const post: Post={title:postForm.value.title, content:postForm.value.content}
-    this.postCreated.emit(post);
+    this.postsService.addPost(postForm.value.title, postForm.value.content);
     console.log(post);
   }
 
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
@@ -7,16 +8,17 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
-  @Input() posts=[];
+  posts=[];
 
   // posts = [
   //   // {title:'First Post', content:'First Post content'},
   //   // {title:'Second Post', content:'Second Post content'},
   //   // {title:'Third Post', content:'Third Post content'}
   // ];
-  constructor() { }
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.posts=this.postsService.getPosts();
   }
 
 }
