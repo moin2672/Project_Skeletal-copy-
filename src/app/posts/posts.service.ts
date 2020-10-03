979545b0d61c58d745103dc41e9476f3a75c1b0c
moin2672 <moin2672@gmail.com>
2020-10-03 16:29:27 +0530

    26. updating post id from null to the new created_post_id

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -36,9 +36,11 @@ export class PostsService {
 
   addPost(title: string, content: string){
     const post:Post= {id:null, title: title, content: content};
-    this.httpClient.post<{message:string}>("http://localhost:3002/api/posts", post)
+    this.httpClient.post<{message:string, postId: string}>("http://localhost:3002/api/posts", post)
           .subscribe((responseData)=>{
-            console.log(responseData.message);
+            // console.log(responseData.message);
+            const id = responseData.postId;
+            post.id=id;
             this.posts.push(post);
             this.postsUpdated.next([...this.posts]);
           })
