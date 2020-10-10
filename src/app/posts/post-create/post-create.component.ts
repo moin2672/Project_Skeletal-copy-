a8a60785812bc060cb53d6cde86023980d67d0f3
moin2672 <moin2672@gmail.com>
2020-10-10 17:21:58 +0530

    39. Uploading files

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -78,7 +78,7 @@ export class PostCreateComponent implements OnInit {
     }
     this.isLoading=true;
     if(!this.editMode){
-      this.postsService.addPost(this.form.value.title, this.form.value.content);
+      this.postsService.addPost(this.form.value.title, this.form.value.content, this.form.value.image);
     } else {
       this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
     }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -41,13 +41,16 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
-  addPost(title: string, content: string){
-    const post:Post= {id:null, title: title, content: content};
-    this.httpClient.post<{message:string, postId: string}>("http://localhost:3002/api/posts", post)
+  addPost(title: string, content: string, image:File){
+    const postData=new FormData();
+    postData.append("title",title);
+    postData.append("content", content)
+    postData.append("image", image, title)
+
+    this.httpClient.post<{message:string, postId: string}>("http://localhost:3002/api/posts", postData)
           .subscribe((responseData)=>{
             // console.log(responseData.message);
-            const id = responseData.postId;
-            post.id=id;
+            const post : Post ={id:responseData.postId, title:title, content:content}
             this.posts.push(post);
             this.postsUpdated.next([...this.posts]);
             this.router.navigate(['/']);
