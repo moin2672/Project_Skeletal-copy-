0b0203e6245d1d19e351a02d792fcab8fdd2ed1c
moin2672 <moin2672@gmail.com>
2020-10-10 14:49:30 +0530

    37. mime validator

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,6 +3,7 @@ import { EmailValidator, FormControl, FormGroup, RequiredValidator, Validators }
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import {Post} from '../post.model';
 import { PostsService } from '../posts.service';
+import {mimeType} from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
@@ -27,7 +28,7 @@ export class PostCreateComponent implements OnInit {
     this.form=new FormGroup({
       title:new FormControl(null, {validators:[Validators.required, Validators.minLength(3)]}),
       content:new FormControl(null, {validators:[Validators.required]}),
-      image:new FormControl(null, {validators:[Validators.required]})
+      image:new FormControl(null, {validators:[Validators.required],asyncValidators:[mimeType]})
     })
 
     this.activatedRoute.paramMap.subscribe((paramMap: ParamMap)=>{
