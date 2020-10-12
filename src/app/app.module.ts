bee4b07a7d3a11f3fc79dd3f098264aa5cbe7bd6
moin2672 <moin2672@gmail.com>
2020-10-12 17:48:42 +0530

    73. creating AngularMaterial Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import{HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatDialogModule} from '@angular/material/dialog';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +16,7 @@ import { SignupComponent } from './auth/signup/signup.component';
 import {AuthInterceptor} from './auth/auth-interceptor'
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component';
+import { AngularMaterialModule } from './angular-material.module';
 
 
 
@@ -41,18 +35,11 @@ import { ErrorComponent } from './error/error.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    AngularMaterialModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    ReactiveFormsModule,
-    MatToolbarModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCardModule,
-    MatExpansionModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    MatDialogModule
+    ReactiveFormsModule
   ],
   providers: [{provide:HTTP_INTERCEPTORS, useClass:AuthInterceptor, multi:true},
               {provide:HTTP_INTERCEPTORS, useClass:ErrorInterceptor, multi:true}],
