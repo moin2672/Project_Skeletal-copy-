7fd2b578abd5dc386344373c62a53386c3a632e0
moin2672 <moin2672@gmail.com>
2020-10-12 10:05:05 +0530

    62. getting userId from the new postSchema

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -26,13 +26,15 @@ export class PostsService {
                   title:post.title,
                   content:post.content,
                   id:post._id,
-                  imagePath:post.imagePath
+                  imagePath:post.imagePath,
+                  creator: post.creator
                 };
               }),
               maxPosts:postData.maxPosts
             };
           }))
           .subscribe(transformedPostData=>{
+            // console.log(transformedPostData)
             this.posts=transformedPostData.posts;
             console.log({
               posts:[...this.posts],
