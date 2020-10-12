e082d3a256cad141ed92e17b0e1cb0f1a0d18fa2
moin2672 <moin2672@gmail.com>
2020-10-12 18:03:22 +0530

    74. creating Post Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import{HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { PostListComponent } from './posts/post-list/post-list.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import {AuthInterceptor} from './auth/auth-interceptor'
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component';
 import { AngularMaterialModule } from './angular-material.module';
+import { PostsModule } from './posts/posts.module';
 
 
 
@@ -24,22 +21,18 @@ import { AngularMaterialModule } from './angular-material.module';
   declarations: [
     AppComponent,
     HeaderComponent,
-    PostCreateComponent,
-    PostListComponent,
     LoginComponent,
     SignupComponent,
     ErrorComponent,
-    
-    
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AngularMaterialModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    ReactiveFormsModule
+    AngularMaterialModule,
+    PostsModule
   ],
   providers: [{provide:HTTP_INTERCEPTORS, useClass:AuthInterceptor, multi:true},
               {provide:HTTP_INTERCEPTORS, useClass:ErrorInterceptor, multi:true}],
